e7d265179a2b78e14dd9460c800c0035541440ea
Seth Akoto <sethakotodid@gmail.com>
2024-04-14 19:17:59 +0000

    updates

--- src/Sections/Categories.jsx
+++ src/Sections/Categories.jsx
@@ -1,10 +1,16 @@
+import CategoriesCard from '../Components/CategoriesCard'
+import categoriesData from '../Constants/CatergoryIcons.json'
 
 const Categories = () => {
   return (
-    <div className=' mx-12 mt-16' >
+    <section className=' mx-12 mt-16' >
         <h1 className=' text-green-600 text-4xl font-normal hover:cursor-text '>Categories</h1>
-        
-    </div>
+        <div className=' flex justify-center  mt-5 gap-12'>
+        {categoriesData.map((categories) => 
+       <CategoriesCard key={categories.id} imgURL={categories.icon} caption={categories.caption} label={categories.desciption}/>  
+    )}
+        </div>
+    </section>
   )
 }
 
