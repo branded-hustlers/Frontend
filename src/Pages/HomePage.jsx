02b4431509deee9b3d0faa66581efaa3e1c8b9da
Seth Akoto <sethakotodid@gmail.com>
2024-04-17 18:00:31 +0000

    updates

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../Layouts/Navbar"
 import BestSellers from "../Sections/BestSellers"
 import Categories from "../Sections/Categories"
+import Footer from "../Sections/Footer"
 import Hero from "../Sections/Hero"
 import LatestProducts from "../Sections/LatestProducts"
 import OnSale from "../Sections/OnSale"
 import Recommended from "../Sections/Recommended"
+import StatBanner from "../Sections/StatBanner"
 
 const HomePage = () => {
   return (
@@ -16,6 +18,8 @@ const HomePage = () => {
     <LatestProducts />
     <OnSale />
     <Recommended />
+    <StatBanner />
+    <Footer />
     </>
   )
 }
