270d03f6546c8b9c5919aa60e5e31ec4190576a8
Ekow-Davis <paakowdavis@gmail.com>
2024-05-07 17:43:07 +0000

    Updated SignInpage responsiveness

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -18,12 +18,12 @@ const SignInPage = () => {
     <>
 
     <div className="flex h-screen">
-      <div className="flex items-start flex-col flex-grow"> {/* Ensure this div expands to fill available space */}
-        <img className='sm:display-none h-32 w-72 pl-8 pt-4' src="../Images/photo_2024-04-02_22-40-01.jpg" alt="Logo" />
-        <img className='sm:display-none h-[80%] w-[100%]' src="./Images/SignInSignOutImage.png" alt="LoginPicture" />
+      <div className="hidden sm:hidden lg:flex items-start flex-col flex-grow"> {/* Ensure this div expands to fill available space */}
+        <img className='h-32 w-72 pl-8 pt-4' src="../Images/photo_2024-04-02_22-40-01.jpg" alt="Logo" />
+        <img className='h-[80%] w-[100%]' src="./Images/SignInSignOutImage.png" alt="LoginPicture" />
       </div>
       <div>
-        <div className='flex flex-grow w-[300px] lg:w-[750px] rounded-tl-[65px] rounded-bl-[65px] bg-green-200 flex-col items-center justify-between px-14'>
+        <div className='flex flex-grow w-[750px] lg:rounded-tr-[1px] lg:rounded-br-[1px] rounded-tl-[65px] rounded-bl-[65px] md:rounded-tl-[65px] md:rounded-bl-[65px] sm:rounded-tr-[65px] sm:rounded-br-[65px] bg-green-200 flex-col items-center justify-between px-14'>
         <h1 className='text-green-700 text-5xl mt-24 mb-16 font-extrabold'>Sign In</h1>
             <form>            
           
